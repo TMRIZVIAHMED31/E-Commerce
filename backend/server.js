470fcdd4c00525c6eab515c4f66c311e462b33a0
T.M.Rizvi Ahmed <tmrizvi59@gmail.com>
2026-09-14 00:30:52 +0600

    Return status from API root endpoint

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.get('/', (req, res) => {
   res.json({
-    message: '"status": "ok"',
+    "status": "ok",
   });
 });
 
